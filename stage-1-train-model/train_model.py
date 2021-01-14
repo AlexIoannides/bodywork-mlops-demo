688b667dab4f8b2c80f7d4acd0290fea1e656d8b
alexioannides <alex.ioanndies@yahoo.co.uk>
2021-01-14 21:35:18 +0000

    Complete stage-1-train-model

--- stage-1-train-model/train_model.py
+++ stage-1-train-model/train_model.py
@@ -1,115 +1,123 @@
 """
 This module defines what will happen in 'stage-1-train-model':
 
-- download dataset;
-- pre-process data into features and labels;
-- train machine learning model; and,
-- save model to cloud stirage (AWS S3).
+- download latest dataset;
+- train machine learning model and compute metrics; and,
+- save model and metrics to cloud storage (AWS S3).
 """
-from datetime import datetime
+import re
+from datetime import datetime, date
 from urllib.request import urlopen
 from typing import Tuple
 
 import boto3 as aws
 import numpy as np
 import pandas as pd
+from botocore.exceptions import ClientError
 from joblib import dump
 from sklearn.base import BaseEstimator
-from sklearn.metrics import f1_score, balanced_accuracy_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_percentage_error, max_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
 
-DATA_URL = ('http://bodywork-ml-pipeline-project.s3.eu-west-2.amazonaws.com'
-            '/data/iris_classification_data.csv')
-TRAINED_MODEL_AWS_BUCKET = 'bodywork-ml-pipeline-project'
-TRAINED_MODEL_FILENAME = 'iris_tree_classifier.joblib'
+AWS_S3_BUCKET = 'bodywork-ml-ops-project'
 
 
 def main() -> None:
     """Main script to be executed."""
-    data = download_dataset(DATA_URL)
-    features, labels = pre_process_data(data)
-    trained_model = train_model(features, labels)
-    persist_model(trained_model)
+    data, data_date = download_latest_dataset(AWS_S3_BUCKET)
+    model, metrics = train_model(data)
+    persist_model(model, data_date)
+    persist_metrics(metrics, data_date)
 
 
-def download_dataset(url: str) -> pd.DataFrame:
-    """Get data from cloud object storage."""
-    print(f'downloading training data from {DATA_URL}')
-    data_file = urlopen(url)
-    return pd.read_csv(data_file)
+def download_latest_dataset(aws_bucket: str) -> Tuple[pd.DataFrame, date]:
+    """Get latest dataset from AWS S3 bucket."""
+    def _date_from_object_key(key: str) -> date:
+        """Extract date from S3 file object key."""
+        date_string = re.findall('20[2-9][0-9]-[0-1][0-9]-[0-3][0-9]', key)[0]
+        file_date = datetime.strptime(date_string, '%Y-%m-%d').date()
+        return file_date
+
+    print(f'downloading training data from s3://{aws_bucket}/datasets')
+    try:
+        s3_client = aws.client('s3')
+        s3_objects = s3_client.list_objects(Bucket=aws_bucket, Prefix='datasets/')
+        object_keys_and_dates = [
+            (obj['Key'], _date_from_object_key(obj['Key']))
+            for obj in s3_objects['Contents']
+        ]
+        latest_dataset_obj = sorted(object_keys_and_dates, key=lambda e: e[1])[-1]
+        latest_dataset_obj_key = latest_dataset_obj[0]
+        object_data = s3_client.get_object(Bucket=aws_bucket, Key=latest_dataset_obj_key)
+        dataset = pd.read_csv(object_data['Body'])
+        dataset_date = latest_dataset_obj[1]
+    except ClientError as e:
+        print(f'failed to download training data from s3://{aws_bucket}/datasets')
+    return (dataset, dataset_date)
 
 
-def pre_process_data(data: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
-    """Prepare raw data for model training."""
-    label_column = 'species'
-    feature_columns = [
-        'sepal length (cm)',
-        'sepal width (cm)',
-        'petal length (cm)',
-        'petal width (cm)'
-    ]
-    classes_map = {'setosa': 0, 'versicolor': 1, 'virginica': 2}
-    X = data[feature_columns].values
-    y = data[label_column].apply(lambda e: classes_map[e]).values
-    return X, y
+def model_metrics(y_actual, y_predicted) -> pd.DataFrame:
+    """Return regression metrics record."""
+    mape = mean_absolute_percentage_error(y_actual, y_predicted)
+    r_squared = r2_score(y_actual, y_predicted)
+    max_residual = max_error(y_actual, y_predicted)
+    metrics_record = pd.DataFrame({
+        'MAPE': [mape],
+        'R2': [r_squared],
+        'MR': [max_residual]
+    })
+    return metrics_record
 
 
-def log_model_metrics_to_stdout(
-    y_actual: np.ndarray,
-    y_predicted: np.ndarray
-) -> None:
-    """Print model evaluation metrics to stdout."""
-    time_now = datetime.now().isoformat(timespec='seconds')
-    accuracy = balanced_accuracy_score(
-        y_actual,
-        y_predicted,
-        adjusted=True
-    )
-    f1 = f1_score(
-        y_actual,
-        y_predicted,
-        average='weighted'
-    )
-    print(f'iris model metrics @{time_now}')
-    print(f' |-- accuracy = {accuracy:.3f}')
-    print(f' |-- f1 = {f1:.3f}')
+def train_model(data:pd.DataFrame) -> Tuple[BaseEstimator, pd.DataFrame]:
+    """Train regression model and compute metrics."""
+    X = data['X'].values.reshape(-1, 1)
+    y = data['y'].values
 
-
-def train_model(features: np.ndarray, labels: np.ndarray) -> BaseEstimator:
-    """Train ML model."""
     X_train, X_test, y_train, y_test = train_test_split(
-        features,
-        labels,
-        test_size=0.1,
-        stratify=labels,
+        X,
+        y,
+        test_size=0.2,
         random_state=42
     )
-    print('training iris decision tree classifier')
-    iris_tree_classifier = DecisionTreeClassifier(
-        class_weight='balanced',
-        random_state=42
-    )
-    iris_tree_classifier.fit(X_train, y_train)
-    test_data_predictions = iris_tree_classifier.predict(X_test)
-    log_model_metrics_to_stdout(y_test, test_data_predictions)
-    return iris_tree_classifier
+
+    ols_regressor = LinearRegression(fit_intercept=True)
+    ols_regressor.fit(X_train, y_train)
+    metrics = model_metrics(y_test, ols_regressor.predict(X_test))
+    return (ols_regressor, metrics)
 
 
-def persist_model(model: BaseEstimator) -> None:
-    """Put trained model into cloud object storage."""
-    dump(model, TRAINED_MODEL_FILENAME)
+def persist_model(model: BaseEstimator, data_date: date) -> None:
+    """Upload trained model to AWS S3."""
+    model_filename = f'regressor-{data_date}.joblib'
+    dump(model, model_filename)
     try:
         s3_client = aws.client('s3')
         s3_client.upload_file(
-            TRAINED_MODEL_FILENAME,
-            TRAINED_MODEL_AWS_BUCKET,
-            f'models/{TRAINED_MODEL_FILENAME}'
+            model_filename,
+            AWS_S3_BUCKET,
+            f'models/{model_filename}'
         )
-        print(f'model saved to s3://{TRAINED_MODEL_AWS_BUCKET}'
-              f'/{TRAINED_MODEL_FILENAME}')
-    except Exception:
+        print(f'uploaded {model_filename} to s3://{AWS_S3_BUCKET}/models/')
+    except ClientError:
         print('could not upload model to S3 - check AWS credentials')
+
+
+def persist_metrics(metrics: pd.DataFrame, data_date: date) -> None:
+    """Upload model metrics to AWS S3."""
+    metrics_filename = f'regressor-{data_date}.csv'
+    metrics.to_csv(metrics_filename, header=True, index=False)
+    try:
+        s3_client = aws.client('s3')
+        s3_client.upload_file(
+            metrics_filename,
+            AWS_S3_BUCKET,
+            f'model-metrics/{metrics_filename}'
+        )
+        print(f'uploaded {metrics_filename} to s3://{AWS_S3_BUCKET}/model-metrics/')
+    except ClientError:
+        print('could not upload model metrics to S3 - check AWS credentials')
 
 
 if __name__ == '__main__':
