56536b5492e5ecec10e18065e9cc063a1803bf47
alexioannides <alex.ioanndies@yahoo.co.uk>
2021-03-01 16:59:43 +0000

    Add dynamic alpha parameter

--- stage-3-generate-next-dataset/synthetic_data_generation.py
+++ stage-3-generate-next-dataset/synthetic_data_generation.py
@@ -10,26 +10,30 @@ import pandas as pd
 from botocore.exceptions import ClientError
 
 AWS_S3_BUCKET = 'bodywork-ml-ops-project'
-
-ALPHA = 30
-BETA = 0.5
-SIGMA = 10
 N = 24 * 60
 
 
 def main() -> None:
     """Main script to be executed."""
-    dataset = generate_dataset(ALPHA, BETA, SIGMA, N)
+    dataset = generate_dataset(N)
     persist_dataset(dataset, AWS_S3_BUCKET)
 
 
-def generate_dataset(alpha: float, beta: float, sigma: float, n: int) -> pd.DataFrame:
+def generate_dataset(n: int) -> pd.DataFrame:
     """Create synthetic regression data using linear model with Gaussian noise."""
-    datestr = np.full(n, str(date.today()))
+
+    def alpha(day_of_year: int, f: float, kappa: float, A: float) -> float:
+        """Return alpha on a given day of the year from a sinusoid model."""
+        return kappa + A * np.sin(2 * np.pi * f * (day_of_year - 1) / 364)
+
+    today = date.today()
+    beta = 0.5
+    sigma = 10
+    alpha_now = alpha(today.timetuple().tm_yday, 6, 1, 0.5)
     X = np.random.uniform(0, 100, n)
     epsilon = np.random.normal(0, 1, n)
-    y = alpha + beta * X + sigma * epsilon
-    dataset = pd.DataFrame({'date': datestr, 'y': y, 'X': X})
+    y = alpha_now + beta * X + sigma * epsilon
+    dataset = pd.DataFrame({'date': np.full(n, str(today)), 'y': y, 'X': X})
     return dataset.query('y >= 0')
 
 
